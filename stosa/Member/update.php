902a0ba64372f910e3b485a0acb7d8397c23d349
Wisal <wisal.sandeepa@gmail.com>
2021-10-06 11:46:10 +0530

    added member update file

---
+++ stosa/Member/update.php
@@ -0,0 +1,47 @@
+<?php
+//headers
+header("Acess-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: PUT");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+//include database and object files
+include_once "../config/database.php";
+include_once "../Objects/member.php";
+
+//instantiate database and connect
+$database = new Database();
+$db = $database->connection();
+
+//initialize object
+$member = new Member($db);
+
+//get raw posted data
+$data = json_decode(file_get_contents("php://input"));
+
+$member->id = $data->id;
+//set member property values to data
+$member->firstName = $data->firstName;
+$member->lastName = $data->lastName;
+$member->email = $data->email;
+$member->addressLine = $data->addressLine;
+$member->city = $data->city;
+
+
+if($member->update()){
+    //set response code -201 created
+    http_response_code(201);
+
+    // tell the user
+    echo json_encode(array("message" => "Member was Updated."));
+}
+
+else{
+    // set response code - 503 service unavailable
+    http_response_code(503);
+  
+    // tell the user
+    echo json_encode(array("message" => "Unable to Update Member."));
+}
+?>
